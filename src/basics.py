40e1f9b9f5a3c17b0be374ca50241a17c91496f3
leon_ce <cedric.leonard@dlr.de>
2025-09-09 13:19:28 +0200

    Updated default basics config to have dataset but no model

---
+++ src/basics.py
@@ -0,0 +1,9 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+@hydra.main(version_base=None, config_path="../configs", config_name="basics.yaml")
+def my_app(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
+if __name__ == "__main__":
+    my_app()
